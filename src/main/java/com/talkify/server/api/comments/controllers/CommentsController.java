c4dac121b8bc0ab8a5287bfd1ef73ddfc5703474
JohnPetros <joaopedro.nc@outlook.com>
2025-02-19 06:30:36 -0300

    🛜 api: add CommentsController

---
+++ src/main/java/com/talkify/server/api/comments/controllers/CommentsController.java
@@ -0,0 +1,17 @@
+package com.talkify.server.api.comments.controllers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@RestController
+@RequestMapping("comments")
+public @interface CommentsController {
+
+}
