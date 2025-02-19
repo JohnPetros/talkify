a54407d0a5fb3391de4043bb9ac83f1f3d904c3a
JohnPetros <joaopedro.nc@outlook.com>
2025-02-19 20:14:02 -0300

    💾 database: add talker model

---
+++ src/main/java/com/talkify/server/database/models/TalkerModel.java
@@ -0,0 +1,30 @@
+package com.talkify.server.database.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "talkers")
+public class TalkerModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String email;
+}
