cba371b2c18684c9152aed0d0f24bc1c0af34a28
JohnPetros <joaopedro.nc@outlook.com>
2025-02-19 20:14:13 -0300

    💾 database: add comment model

---
+++ src/main/java/com/talkify/server/database/models/CommentModel.java
@@ -0,0 +1,43 @@
+package com.talkify.server.database.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "comments")
+public class CommentModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column
+  private String content;
+
+  @Column(name = "document_id")
+  private UUID documentId;
+
+  @ManyToOne
+  @JoinColumn(name = "talker_id")
+  private TalkerModel talker;
+
+  @Column(name = "posted_at")
+  @CreationTimestamp
+  private LocalDateTime postedAt;
+}
