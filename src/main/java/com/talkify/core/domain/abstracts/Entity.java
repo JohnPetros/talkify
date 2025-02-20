e7bec29ae7d4cdf6e42c5120bb0b3679e9c327b0
JohnPetros <joaopedro.nc@outlook.com>
2025-02-20 06:21:43 -0300

    🩻 abstracts: implement hash cide and equals for all entities

--- src/main/java/com/talkify/core/domain/abstracts/Entity.java
+++ src/main/java/com/talkify/core/domain/abstracts/Entity.java
@@ -1,19 +1,53 @@
 package com.talkify.core.domain.abstracts;
 
+import java.lang.reflect.Field;
+import java.util.Objects;
 import java.util.UUID;
 
 public abstract class Entity {
-  private final String id;
+  private final UUID id;
 
   protected Entity(String id) {
-    this.id = (id != null) ? id : UUID.randomUUID().toString();
+    this.id = (id != null) ? UUID.fromString(id) : UUID.randomUUID();
   }
 
-  public String getId() {
+  public UUID getId() {
     return id;
   }
 
   public Boolean isEqualTo(Entity entity) {
     return id == entity.id;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    Entity entity = (Entity) object;
+    return Objects.equals(getId(), entity.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    Field[] fields = this.getClass().getDeclaredFields();
+    StringBuilder fieldsString = new StringBuilder("{ ");
+
+    for (Field field : fields) {
+      field.setAccessible(true);
+      try {
+        fieldsString.append(field.getName()).append(": ").append(field.get(this)).append(", ");
+      } catch (IllegalAccessException e) {
+        fieldsString.append(field.getName()).append(": <inaccessible>, ");
+      }
+    }
+
+    if (fields.length > 0) {
+      fieldsString.setLength(fieldsString.length() - 2);
+    }
+
+    return fieldsString.append(" }").toString();
   }
 }
