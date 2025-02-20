7697ce13132a99ad9841776817bd40a96f8d7632
JohnPetros <joaopedro.nc@outlook.com>
2025-02-20 06:18:41 -0300

    🚨 exceptions: add AppException

---
+++ src/main/java/com/talkify/core/domain/exceptions/AppException.java
@@ -0,0 +1,20 @@
+package com.talkify.core.domain.exceptions;
+
+public class AppException extends RuntimeException {
+  private final String title;
+  private final String message;
+
+  public AppException(String title, String message) {
+    super(message);
+    this.title = title;
+    this.message = message;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
