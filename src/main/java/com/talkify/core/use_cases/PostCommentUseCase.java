b210138973ea766569cc28c87b7b2f6e0f4922cc
JohnPetros <joaopedro.nc@outlook.com>
2025-02-20 06:22:16 -0300

    ✨ use cases: post comment

---
+++ src/main/java/com/talkify/core/use_cases/PostCommentUseCase.java
@@ -0,0 +1,34 @@
+package com.talkify.core.use_cases;
+
+import com.talkify.core.domain.dtos.CommentDto;
+import com.talkify.core.domain.entities.Comment;
+import com.talkify.core.domain.entities.Talker;
+import com.talkify.core.interfaces.repositories.CommentsRepository;
+import com.talkify.core.interfaces.repositories.TalkersRepository;
+
+public class PostCommentUseCase {
+  CommentsRepository commentsRepository;
+  TalkersRepository talkersRepository;
+
+  public PostCommentUseCase(CommentsRepository commentsRepository, TalkersRepository talkersRepository) {
+    this.commentsRepository = commentsRepository;
+    this.talkersRepository = talkersRepository;
+  }
+
+  public void execute(CommentDto commentDto, String talkerId, String documentId) {
+    var currentTalker = talkersRepository.findById(talkerId);
+    Talker commentTalker;
+    System.out.println(currentTalker.isEmpty());
+    if (currentTalker.isEmpty()) {
+      commentTalker = new Talker(commentDto.talker);
+      // talkersRepository.add(commentTalker);
+    } else {
+      commentTalker = currentTalker.get();
+    }
+
+    commentDto.setTalker(commentTalker.getDto());
+    var comment = new Comment(commentDto);
+
+    System.out.println(comment);
+  }
+}
