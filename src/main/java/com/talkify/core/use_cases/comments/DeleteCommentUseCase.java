4b331c14c05d432e257615659abe7eb354a3105c
JohnPetros <joaopedro.nc@outlook.com>
2025-02-24 23:58:41 -0300

    ✨ use cases: delete comment

---
+++ src/main/java/com/talkify/core/use_cases/comments/DeleteCommentUseCase.java
@@ -0,0 +1,26 @@
+package com.talkify.core.use_cases.comments;
+
+import com.talkify.core.domain.entities.Comment;
+import com.talkify.core.domain.exceptions.NotFoundException;
+import com.talkify.core.interfaces.repositories.CommentsRepository;
+
+public class DeleteCommentUseCase {
+  CommentsRepository repository;
+
+  public DeleteCommentUseCase(CommentsRepository repository) {
+    this.repository = repository;
+  }
+
+  public void execute(String commentId) {
+    var comment = findComment(commentId);
+    repository.delete(comment);
+  }
+
+  private Comment findComment(String commentId) {
+    var comment = repository.findById(commentId);
+    if (comment.isEmpty()) {
+      throw new NotFoundException("Comentário não encontrado");
+    }
+    return comment.get();
+  }
+}
