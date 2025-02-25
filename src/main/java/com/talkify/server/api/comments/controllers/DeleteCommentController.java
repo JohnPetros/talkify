30483d642d1b10231c1aea3e80c59f1e27907c15
JohnPetros <joaopedro.nc@outlook.com>
2025-02-24 23:59:08 -0300

    🛜 api: add DeleteCommentController

---
+++ src/main/java/com/talkify/server/api/comments/controllers/DeleteCommentController.java
@@ -0,0 +1,29 @@
+package com.talkify.server.api.comments.controllers;
+
+import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+
+import com.talkify.core.interfaces.repositories.CommentsRepository;
+import com.talkify.core.use_cases.comments.DeleteCommentUseCase;
+
+@Data
+class Body {
+  private String commentId;
+}
+
+@CommentsController
+public class DeleteCommentController {
+  @Autowired
+  private CommentsRepository repository;
+
+  @DeleteMapping
+  public ResponseEntity<Void> handle(@RequestBody Body body) {
+    var useCase = new DeleteCommentUseCase(repository);
+    useCase.execute(body.getCommentId());
+    return ResponseEntity.noContent().build();
+  }
+}
