1ce9aa693adba3b2c1ca5093f192e4ccfd05bf7a
JohnPetros <joaopedro.nc@outlook.com>
2025-02-28 15:35:18 -0300

    🛜 api: add ListCommentsControlle

--- src/main/java/com/talkify/server/api/comments/controllers/ListCommentsControlle.java
+++ src/main/java/com/talkify/server/api/comments/controllers/ListCommentsControlle.java
@@ -1,15 +1,29 @@
 package com.talkify.server.api.comments.controllers;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.talkify.core.domain.dtos.CommentDto;
+import com.talkify.core.interfaces.repositories.CommentsRepository;
+import com.talkify.core.responses.PaginationResponse;
+import com.talkify.core.use_cases.comments.ListCommentsUseCase;
 
 @CommentsController
 public class ListCommentsControlle {
+  @Autowired
+  private CommentsRepository commentsRepository;
+
   @GetMapping
-  public ResponseEntity<List<String>> handle() {
-    return ResponseEntity.status(HttpStatus.CREATED).body(List.of());
+  public ResponseEntity<PaginationResponse<CommentDto>> handle(
+      @RequestParam(defaultValue = "1") int page,
+      @RequestParam(defaultValue = "10") int itemsPerPage) {
+
+    var useCase = new ListCommentsUseCase(commentsRepository);
+    var comments = useCase.execute(page, itemsPerPage);
+    var response = new PaginationResponse<CommentDto>(comments, itemsPerPage);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 }
