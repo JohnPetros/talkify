3408f709b8c4cb1b429bf91ab73fccd00cf58a82
JohnPetros <joaopedro.nc@outlook.com>
2025-03-02 19:16:11 -0300

    💾 database: add AccountModel

---
+++ src/main/java/com/talkify/server/database/mappers/JpaAccountMapper.java
@@ -0,0 +1,32 @@
+package com.talkify.server.database.mappers;
+
+import org.springframework.stereotype.Service;
+
+import com.talkify.core.domain.dtos.AccountDto;
+import com.talkify.core.domain.entities.Account;
+import com.talkify.server.database.models.AccountModel;
+
+@Service
+public class JpaAccountMapper {
+  public Account toEntity(AccountModel model) {
+    var dto = new AccountDto().setId(null)
+        .setName(model.getName())
+        .setEmail(model.getEmail())
+        .setPassword(model.getPassword())
+        .setRole(model.getRole().toString());
+
+    return new Account(dto);
+  }
+
+  public AccountModel toModel(Account account) {
+    var model = AccountModel
+        .builder()
+        .id(account.getId().value())
+        .name(account.getName().value())
+        .email(account.getEmail().value())
+        .password(account.getPassword().value())
+        .build();
+
+    return model;
+  }
+}
