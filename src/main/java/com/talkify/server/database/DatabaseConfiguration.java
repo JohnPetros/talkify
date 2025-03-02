b4265b35b7fc56a3c046b6075a06d9895b5ff067
JohnPetros <joaopedro.nc@outlook.com>
2025-03-02 19:17:28 -0300

    💾 database: export JpaAccountsRepository to all app

--- src/main/java/com/talkify/server/database/DatabaseConfiguration.java
+++ src/main/java/com/talkify/server/database/DatabaseConfiguration.java
@@ -3,7 +3,9 @@ package com.talkify.server.database;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.talkify.core.interfaces.repositories.AccountsRepository;
 import com.talkify.core.interfaces.repositories.CommentsRepository;
+import com.talkify.server.database.repositories.JpaAccountsRepository;
 import com.talkify.server.database.repositories.JpaCommentsRepository;
 
 @Configuration
@@ -11,5 +13,10 @@ public class DatabaseConfiguration {
   @Bean
   CommentsRepository commentsRepository() {
     return new JpaCommentsRepository();
+  }
+
+  @Bean
+  AccountsRepository accountsRepository() {
+    return new JpaAccountsRepository();
   }
 }
